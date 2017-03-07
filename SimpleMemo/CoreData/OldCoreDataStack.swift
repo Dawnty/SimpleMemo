e69cf5234e56d789078771be7b5b6b28325a96c3
lijun <lijun01@douban.com>
2017-03-07 08:08:29 +0800

    fix release crash

--- SimpleMemo/CoreData/OldCoreDataStack.swift
+++ SimpleMemo/CoreData/OldCoreDataStack.swift
@@ -20,16 +20,19 @@ class OldCoreDataStack: NSObject {
     return urls[urls.count-1]
   }()
 
-  lazy var managedObjectModel: NSManagedObjectModel = {
+  lazy var managedObjectModel: NSManagedObjectModel? = {
     // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-    let modelURL = Bundle.main.url(forResource: "Memo", withExtension: "momd")!
-    return NSManagedObjectModel(contentsOf: modelURL)!
+    if let modelURL = Bundle.main.url(forResource: "Memo", withExtension: "momd") {
+      return NSManagedObjectModel(contentsOf: modelURL)
+    }
+    return nil
   }()
 
   lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
     // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
     // Create the coordinator and store
-    var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+    guard let modle = self.managedObjectModel else { return nil }
+    var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: modle)
     let url = self.applicationDocumentsDirectory.appendingPathComponent("Memo.sqlite")
     var failureReason = "There was an error creating or loading the application's saved data."
     do {
