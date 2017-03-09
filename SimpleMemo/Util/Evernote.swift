2eae9dd1cf36a6067eb5662ae2b532fde558114b
李俊 <lijun@lijundeiMac.local>
2017-03-09 08:59:33 +0800

    fetchOrCreateSimpleMemoNoteBook

--- SimpleMemo/Util/Evernote.swift
+++ SimpleMemo/Util/Evernote.swift
@@ -11,6 +11,52 @@ import EvernoteSDK
 
 extension ENSession {
 
+  func fetchOrCreateSimpleMemoNoteBook() -> EDAMNotebook? {
+    if !self.isAuthenticated { return nil }
+    guard let client = self.primaryNoteStore() else { return nil }
+
+    if let bookGuid: String = UserDefaults.standard.object(forKey: "SimpleMemoNoteBook") as? String {
+      client.fetchNotebook(withGuid: bookGuid, completion: { (book, error) in
+        if let book = book {
+          printLog(message: "\(book)")
+        } else if let error = error {
+          printLog(message: error.localizedDescription)
+        }
+      })
+    }
+
+    let noteBook = EDAMNotebook()
+    noteBook.name = "易便签"
+    client.create(noteBook) { (book, error) in
+      if let book = book {
+        printLog(message: book.name)
+      }
+
+      if let error = error {
+        printLog(message: error.localizedDescription)
+        self.fetchSimpleMemoNoteBook()
+      }
+    }
+
+    return nil
+  }
+
+  func fetchSimpleMemoNoteBook() -> EDAMNotebook? {
+    guard let client = self.primaryNoteStore() else { return nil }
+    client.listNotebooks { (books, error) in
+      if let books = books {
+        for book in books {
+          if book.name == "易便签" {
+            UserDefaults.standard.set(book.guid, forKey: "SimpleMemoNoteBook")
+            printLog(message: "\(book)")
+            break
+          }
+        }
+      }
+    }
+    return nil
+  }
+
   /// 上传便签到印象笔记
   func uploadMemoToEvernote(_ memo: Memo) {
     if self.isAuthenticated == false {
